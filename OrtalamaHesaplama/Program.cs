4b670761883ca0f3a57eee78163fe7c0b08d66b2
Baybars Bozok Tanridagli <baybarsstanridagli@gmail.com>
2023-05-26 20:45:01 +0300

    KDV
    Simple KDV tax calculation

---
+++ OrtalamaHesaplama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class KDV
+{
+    static void Main(string[] args)
+    {
+        string sayi;
+        Console.Write("Please first number");
+        sayi = Console.ReadLine();
+        Double KDV = 0.18;
+
+        Console.WriteLine("The number you entered %18 :" + (Convert.ToDouble(sayi) * KDV));
+        Console.ReadKey();
+    }
+}
